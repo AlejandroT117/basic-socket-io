201f3028d8a26a0311a57dff8c82d950cda0809b
AlejandroT117 <ualejandrotrejo@gmail.com>
2022-07-01 15:34:04 -0500

    timer of 30s for sending risklevel by socketIO

--- index.js
+++ index.js
@@ -9,38 +9,46 @@ const { Server } = require("socket.io");
 const server = http.createServer(app);
 const io = new Server(server, {
   // options
-  cors:{
-    origin: ['http://localhost:3000']
-  }
+  cors: {
+    origin: ["http://localhost:3000"],
+  },
 });
+
+const roomsList = [388, 152, 357, 206, 411, 439, 369, 339, 169];
 
 (() => {
   try {
     app.use(cors());
 
-    app.use("/", (req, res)=>{
-      res.send(`Hola Mundo`)
-    })
+    app.use("/", (req, res) => {
+      res.send(`Hola Mundo`);
+    });
+    let reps = 15;
 
     io.on("connection", (socket) => {
       console.log(`New connection: ${socket.id}`);
+
+      setInterval(() => {
+        let number = Math.floor(Math.random() * 3);
+        let roomIndex = Math.floor(Math.random() * 9);
+
+        socket.emit(`room ${roomsList[roomIndex]}`, {'FallRisk': number});
+        console.log(`room ${roomsList[roomIndex]}`, {'FallRisk': number});
+      }, 30000);
 
       socket.on("chat", (msg) => {
         console.log(`Chating: ${msg}`);
       });
 
-      socket.on("room352", (msg) => {
-        console.log(`Receving message from room 352: ${msg}`);
-        socket.emit('chat', 'Message from socketio')
-      });
-
       socket.onAny((event, msg) => {
-        console.log(`got ${event} and msg: ${msg}`);
+        console.log(`Event: ${event} - Msg: ${msg}`);
       });
     });
 
-    server.listen(PORT, () => console.log(`Listening on: http://localhost:${PORT}`));
+    server.listen(PORT, () =>
+      console.log(`Listening on: http://localhost:${PORT}`)
+    );
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 })();
